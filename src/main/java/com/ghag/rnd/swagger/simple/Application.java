a36a0eab97e0a9c485ac8309304f7a6418debf9b
Ganesh1 Ghag <Ganesh1.Ghag@in.ril.com>
2018-06-10 20:21:30 +0530

    added code related to odata2 sample services

--- src/main/java/com/ghag/rnd/swagger/simple/Application.java
+++ src/main/java/com/ghag/rnd/swagger/simple/Application.java
@@ -1,12 +1,18 @@
 package com.ghag.rnd.swagger.simple;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan  //important and required
 @EnableSwagger2
@@ -14,5 +20,18 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean exampleServletBean() {
+	    ServletRegistrationBean bean = new ServletRegistrationBean(
+	      new org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet(), "/MyFormula.svc/*");
+	    bean.setLoadOnStartup(1);
+	    Map<String, String> params = new HashMap<String, String>();
+		params.put("javax.ws.rs.Application", "org.apache.olingo.odata2.core.rest.app.ODataApplication");
+		params.put("org.apache.olingo.odata2.service.factory", "org.apache.olingo.sample.annotation.processor.AnnotationSampleServiceFactory");
+	    bean.setInitParameters(params);
+	    
+	    return bean;
 	}
 }
---
+++ src/main/java/com/ghag/rnd/swagger/simple/CarServiceServlet.java
@@ -0,0 +1,34 @@
+package com.ghag.rnd.swagger.simple;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+//@Configuration
+//public class CarServiceServlet extends SpringBootServletInitializer {
+public class CarServiceServlet {
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CarServiceServlet.class);
+	}
+
+	@Bean
+	public org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet dispatcherServlet() {
+		return new org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet();
+	}
+
+	@Bean
+	public ServletRegistrationBean dispatcherServletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet());
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("javax.ws.rs.Application", "org.apache.olingo.odata2.core.rest.app.ODataApplication");
+		params.put("org.apache.olingo.odata2.service.factory", "org.apache.olingo.sample.annotation.processor.AnnotationSampleServiceFactory");
+		registration.setInitParameters(params);
+		registration.setUrlMappings(Arrays.asList("/MyFormula.svc/*"));
+		return registration;
+	}
+
+}
